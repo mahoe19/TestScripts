ff9410d8f7d2364ff4f1129a11751d6213c56307
Marcus <mahoe19@student.sdu.dk>
2022-05-18 08:41:13 +0200

    changes to analyse.m

--- test_scripts/analyze.m
+++ test_scripts/analyze.m
@@ -2,8 +2,8 @@ delta_t=readmatrix("with_delta_t.csv");
 delta_t=delta_t(:,1);
 delta_t=delta_t(2:end,:);
 delta_t=delta_t*1000;
+delta_t=delta_t.'
 
 close all
-x=-10:.1:10;
-y=sin(x);
-plot(x,y);
+[p,x] = hist(delta_t); 
+plot(x,p/sum(p)); %PDF
